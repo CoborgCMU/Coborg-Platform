e255ecb9cbd7885484e70a09a900c65db8a8ba55
Yuqing Qin <53550898+Sunny-Qin-0314@users.noreply.github.com>
2021-09-27 15:50:08 -0400

    Delete arduino_ws directory

--- arduino_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/coborg/Coborg-Platform/arduino_ws/src
-# Build directory: /home/coborg/Coborg-Platform/arduino_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("cmu_motor_lab")
