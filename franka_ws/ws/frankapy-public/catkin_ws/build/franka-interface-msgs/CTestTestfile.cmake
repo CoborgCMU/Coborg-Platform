bbb222c21f82bbb3a7ac81927ae2aa7ba9225031
themorethegerryer <gdascoli@terpmail.umd.edu>
2021-09-27 15:27:26 -0400

    remove franka_ws from coborg

--- franka_ws/ws/frankapy-public/catkin_ws/build/franka-interface-msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/coborg/Coborg-Platform/franka_ws/ws/frankapy-public/catkin_ws/src/franka-interface-msgs
-# Build directory: /home/coborg/Coborg-Platform/franka_ws/ws/frankapy-public/catkin_ws/build/franka-interface-msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
