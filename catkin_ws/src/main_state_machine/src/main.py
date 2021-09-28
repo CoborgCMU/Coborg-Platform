0bd9d45b2d300ecb93a5428236db0040a4f54d6d
husamwadi <husamwadi2@gmail.com>
2021-09-28 12:53:35 -0400

    Update main.py

--- catkin_ws/src/main_state_machine/src/main.py
+++ catkin_ws/src/main_state_machine/src/main.py
@@ -7,13 +7,17 @@ import enum
 
 # Updates List:
 # Consider changing the state machine to work on actions instead of publisher/subscriber
-# Figure out how to publish sounds to speaker
+# Figure out how to publish sounds to speaker through state outputs
+# Add error checking through state inputs/outputs
+# convert current status -> status outputs
+# add status inputs
 
 # Commands:
+# 0 = pause > Motors stop moving, but maintain with lower torque threshold
 # 1 = stop > Shut off power to motors
 # 2 = target > Move to and hold plate
 # 3 = home > Return to home position
-# # = warning > Motors stop moving, but maintain with lower torque threshold
+
 
 class Command(enum.IntEnum): #these are the voice commands that come in and get sent to the motors
     STOP = 1 
@@ -21,27 +25,26 @@ class Command(enum.IntEnum): #these are the voice commands that come in and get 
     HOME = 3
     WARNING = 4
 
-# Statuses:
-# 1 = initializing > Command received, but not executing yet (e.g. detecting hands) [ROS INFO]
+# Status Outputs:
+# 1 = initializing > Command received, but not executing yet (e.g. detecting hands) 
 # 2 = executing > Command being executed (e.g. moving to target)
-# 3 = waiting > Command completed/performing holding task, ready for next command (maintaining position in 3d space)
+# 3 = idle > Command completed/performing holding task, ready for next command (maintaining position in 3d space)
+# 4. = warning > Issue that is continuable.
+# 5. = error > Problem that requires full reset.
 
 class Status(enum.IntEnum): #this is the status that outputs to ROS terminal
     INIT = 1 
     EXECUTE = 2 
     IDLE = 3
 
-# Speaker Sounds (to be implemented):
-# 0 = start-up > System has launched
-# 1 = listening > Keyword "COBORG" identified, awaiting full command
-# 2 = understood > Voice command understood
-# 3 = not understood > Voice command not understood
-# 4 = emergency > Emergency command detected
-# 5 = waiting > Task completed, available for next command
+# Status Inputs:
+# 0 = Processing > Command recieved and executing
+# 1 = Success > Command completed 
+# 2 = Warning > Command completed but with issues
+# 3 = Error > Command not completed
 
 status = Status.INIT # initializing
 function = Command.HOME  # compact robot arm mode
-
 status = Status.IDLE #idle/ready
 
 # Function for /voice_commands
@@ -95,9 +98,8 @@ if __name__ == "__main__":
     rospy.init_node('main_state_machine')
     voice_commands_sub = rospy.Subscriber('/voice_commands', Int32, new_command)
     state_output_pub = rospy.Publisher('/state_output', Int32, queue_size=1)
-    #speaker_output_pub = rospy.Publisher('/speaker_output', Int32, queue_size=1)
+ 
     state_input_sub = rospy.Subscriber('/state_input', Int32, status_update)
     state_output_pub.publish(function)
-    #speaker_output_pub.publish(0)
-
+    
     rospy.spin() # Should this just be spin()?
